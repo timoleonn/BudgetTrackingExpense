714aefb47fc8054c65a0d89cb494786f8ff74f42
Andreas Christodoulou <57558807+andreaschh@users.noreply.github.com>
2020-11-05 14:33:07 +0200

    test2

--- app/src/main/java/com/example/budgettrackingexpense/MainActivity.java
+++ app/src/main/java/com/example/budgettrackingexpense/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    //kjenke
     }
 
     @Override
