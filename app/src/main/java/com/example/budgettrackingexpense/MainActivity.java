f9fb9fba4cf13038b714a6823987713f0472eb8c
timoleonn <tim.charilaou@gmail.com>
2020-11-07 01:56:55 +0200

    Started working on Drawer Activity Linked settings button

--- app/src/main/java/com/example/budgettrackingexpense/MainActivity.java
+++ app/src/main/java/com/example/budgettrackingexpense/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.budgettrackingexpense;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -52,7 +55,23 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
 
+    //  CHECK IF SETTINGS IS CLICKED
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent in = new Intent(this, Settings.class);
+            startActivity(in);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
