f2b7f4efad2a6104f7f03d1e75ee8791f4c2ceb8
timoleonn <tim.charilaou@gmail.com>
2020-11-10 01:18:28 +0200

    Fixed Navigation Drawer Activity file structure

--- app/src/main/java/com/example/budgettrackingexpense/GalleryFragment.java
+++ app/src/main/java/com/example/budgettrackingexpense/GalleryFragment.java
@@ -1,4 +1,4 @@
-package com.example.budgettrackingexpense.ui.gallery;
+package com.example.budgettrackingexpense;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -16,20 +16,10 @@ import com.example.budgettrackingexpense.R;
 
 public class GalleryFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
