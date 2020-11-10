9279242d352526602f3bc0aa04cffe558fd3bd6f
timoleonn <tim.charilaou@gmail.com>
2020-11-11 00:42:40 +0200

    Revert "Started working on the menus"
    This reverts commit 7c160e71aeb95500e60e174f42449022df02855a.

--- app/src/main/java/com/example/budgettrackingexpense/GalleryFragment.java
+++ app/src/main/java/com/example/budgettrackingexpense/GalleryFragment.java
@@ -11,15 +11,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.budgettrackingexpense.R;
 
-public class HomeFragment extends Fragment {
+public class GalleryFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
 
         return root;
     }
--- app/src/main/java/com/example/budgettrackingexpense/ProfileFragment.java
+++ app/src/main/java/com/example/budgettrackingexpense/ProfileFragment.java
@@ -8,17 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 public class ProfileFragment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
-
-        return root;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
