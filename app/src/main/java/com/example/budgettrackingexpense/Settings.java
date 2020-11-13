84a257d07f6bed8bc2dfeafe64859d8d5aed7aec
Andreas Christodoulou <57558807+andreaschh@users.noreply.github.com>
2020-11-13 15:10:34 +0200

    remove revolut

--- app/src/main/java/com/example/budgettrackingexpense/Settings.java
+++ app/src/main/java/com/example/budgettrackingexpense/Settings.java
@@ -2,7 +2,10 @@ package com.example.budgettrackingexpense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioGroup;
 
