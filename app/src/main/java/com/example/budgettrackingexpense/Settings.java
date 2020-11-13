cb9c932f9472e93f68a508dad662670610016435
timoleonn <tim.charilaou@gmail.com>
2020-11-13 15:30:00 +0200

    Revert "remove revolut"
    This reverts commit 84a257d07f6bed8bc2dfeafe64859d8d5aed7aec.

--- app/src/main/java/com/example/budgettrackingexpense/Settings.java
+++ app/src/main/java/com/example/budgettrackingexpense/Settings.java
@@ -2,10 +2,7 @@ package com.example.budgettrackingexpense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioGroup;
 
