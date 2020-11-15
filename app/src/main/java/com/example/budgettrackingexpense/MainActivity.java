be0694cf452139fb2db1414ff907e6ed56ba88c0
timoleonn <tim.charilaou@gmail.com>
2020-11-15 23:39:19 +0200

    FIXED NAVIGATION DRAWER REMOVED CATEGORIES FRAGMENT

--- app/src/main/java/com/example/budgettrackingexpense/MainActivity.java
+++ app/src/main/java/com/example/budgettrackingexpense/MainActivity.java
@@ -11,6 +11,9 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
@@ -21,12 +24,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    DrawerLayout drawer;
+
     private AppBarConfiguration mAppBarConfiguration;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+
     public static final String FULLNAME = "";
 
     @Override
@@ -37,37 +43,19 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_my_banks,
-                R.id.nav_rate_us)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
 
-//        //  CHECK WHAT MENU ITEM IN NAVIGATION DRAWER IS PRESSED
-//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.nav_categories:
-////                        startActivity(new Intent(getApplicationContext(), CategoriesActivity.class));
-////                        overridePendingTransition(0, 0);
-//                    case R.id.nav_my_banks:
-//                        fragmentManager = getSupportFragmentManager();
-//                        fragmentTransaction = fragmentManager.beginTransaction();
-//                        fragmentTransaction.add(R.id.nav_host_fragment,new MyBanksFragment());
-//                        fragmentTransaction.commit();
-//                }
-//
-//                return false;
-//            }
-//        });
+        if (savedInstanceState == null ) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.nav_host_fragment, new HomeFragment());
+            fragmentTransaction.commit();
+        }
     }
 
     //  CHECK WHAT BUTTON IS PRESSED ON TOOLBAR TOP
@@ -89,10 +77,36 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    //  CHECK WHAT MENU ITEM IN NAVIGATION DRAWER IS PRESSED
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                goToFragment(new HomeFragment());
+            case R.id.nav_categories:
+                startActivity(new Intent(getApplicationContext(), CategoriesActivity.class));
+                overridePendingTransition(0, 0);
+            case R.id.nav_my_banks:
+                goToFragment(new MyBanksFragment());
+            case R.id.nav_all_banks:
+                startActivity(new Intent(getApplicationContext(), Tabbed_Bank_Activity.class));
+                overridePendingTransition(0, 0);
+            case R.id.nav_profile:
+//                goToFragment(new ProfileFragment());
+            case R.id.nav_rate_us:
+                goToFragment(new RateUsFragment());
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
+
+    //  CALL FRAGMENT
+    public void goToFragment(Fragment f) {
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment, f);
+        fragmentTransaction.commit();
+    }
+
+
 }
