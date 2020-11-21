8bcdfbc415a95307a2463c9cd57d584512e1d59d
timoleonn <tim.charilaou@gmail.com>
2020-11-22 01:36:53 +0200

    Categories now are based on current user uid (before it was hard coded)

--- app/src/main/java/com/example/budgettrackingexpense/CategoriesActivity.java
+++ app/src/main/java/com/example/budgettrackingexpense/CategoriesActivity.java
@@ -19,6 +19,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -74,7 +76,13 @@ public class CategoriesActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         categories = new ArrayList<>();
 
-        databaseReference = FirebaseDatabase.getInstance().getReference("users").child("-sdfsdfsdfsdf").child("categories");
+        //  GET CURRENT USER
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //  GET CURRENT USER UID
+        String currentUserUid = currentUser.getUid();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users").child(currentUserUid).child("categories");
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
