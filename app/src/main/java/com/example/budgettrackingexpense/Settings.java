6fb48a3445b6139c6a97d62ee45f6c8dc78745af
Andreas Christodoulou <57558807+andreaschh@users.noreply.github.com>
2020-11-26 14:15:11 +0200

    Revert "Disable Button add epxense"
    This reverts commit d8c160496de111094e09ec8c3e8e14fd6c7c6b62.

--- app/src/main/java/com/example/budgettrackingexpense/Settings.java
+++ app/src/main/java/com/example/budgettrackingexpense/Settings.java
@@ -125,4 +125,20 @@ public class Settings extends AppCompatActivity {
 
 
     }
+        public void theme(View v)
+        {
+            Switch sw = findViewById(R.id.switchBright);
+            LinearLayout layout = findViewById(R.id.LinearLayoutSettings);
+
+            if (sw.isChecked())
+            {
+                layout.setBackgroundColor(Color.BLACK);
+                sw.setText("Dark Mode: On");
+            } else {
+                layout.setBackgroundColor(Color.WHITE);
+                sw.setText("Dark Mode: Off");
+
+            }
+
+        }
 }
