0bdde7858cfb9393cff5cc11f40ab391b6807054
Andreas Christodoulou <57558807+andreaschh@users.noreply.github.com>
2020-11-26 17:44:39 +0200

    last push

--- app/src/main/java/com/example/budgettrackingexpense/Settings.java
+++ app/src/main/java/com/example/budgettrackingexpense/Settings.java
@@ -127,10 +127,9 @@ public class Settings extends AppCompatActivity {
                 fout.write(ans3.getBytes());
                 fout.write(ans4.getBytes());
                 fout.close();
-<<<<<<< HEAD
+
                 System.out.println("2:saved" + ans);
-=======
->>>>>>> origin/master
+
                 System.out.println("2:saved" + ans1);
                 System.out.println("2:saved" + ans2);
                 System.out.println("2:saved" + ans3);
