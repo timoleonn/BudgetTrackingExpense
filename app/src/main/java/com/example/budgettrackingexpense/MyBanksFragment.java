61371ede3ac0276800174479307acfafa0b98ffe
Andreas Christodoulou <57558807+andreaschh@users.noreply.github.com>
2020-11-28 13:04:56 +0200

    Started on myBanks Fragment

--- app/src/main/java/com/example/budgettrackingexpense/MyBanksFragment.java
+++ app/src/main/java/com/example/budgettrackingexpense/MyBanksFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -25,7 +26,16 @@ public class MyBanksFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_my_banks, container, false);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("My Banks");
 
         ArrayList<List> Banks = new ArrayList();
 
@@ -39,10 +49,52 @@ public class MyBanksFragment extends Fragment {
                 String[] res = strLine.split("[,]", 0);
                 System.out.println(res);
                 for (String myStr : res) {
+                    if (myStr.getBytes().equals("Bank of Cyprus"))
+                    {
+                        Button cy = view.findViewById(R.id.cbcyprus);
+                        cy.setEnabled(true);
+
+                    }else {
+                        Button cy = view.findViewById(R.id.cbcyprus);
+                        cy.setEnabled(false);
+                    }
+                    if (myStr.getBytes().equals("AstroBank"))
+                    {
+                        Button astrob = view.findViewById(R.id.cbastro);
+                        astrob.setEnabled(true);
+                    }else {
+                        Button astrob = view.findViewById(R.id.cbastro);
+                        astrob.setEnabled(false);
+                    }
+                    if (myStr.getBytes().equals("Hellenic"))
+                    {
+                        Button hel = view.findViewById(R.id.cbhellenic);
+                        hel.setEnabled(true);
+                    }else {
+                        Button hel = view.findViewById(R.id.cbhellenic);
+                        hel.setEnabled(false);
+                    }
+                    if (myStr.getBytes().equals("RcbBank"))
+                    {
+                        Button rcbB = view.findViewById(R.id.rcbBank);
+                        rcbB.setEnabled(true);
+                    }else {
+                        Button rcbB = view.findViewById(R.id.rcbBank);
+                        rcbB.setEnabled(false);
+                    }
+                    if (myStr.getBytes().equals("Alpha Bank"))
+                    {
+                        Button alphaB = view.findViewById(R.id.cbalpha);
+                        alphaB.setEnabled(true);
+                    }else {
+                        Button alphaB = view.findViewById(R.id.cbalpha);
+                        alphaB.setEnabled(false);
+                    }
                     System.out.println(myStr);
                     //  ADDS THE FOUR VARIABLES TO THE STRING ARRAY
                     Banks.add(Collections.singletonList(myStr));
-                    System.out.println(Banks);
+                    System.out.println("1"+Banks);
+
                 }
             }
         } catch (FileNotFoundException e) {
@@ -51,17 +103,7 @@ public class MyBanksFragment extends Fragment {
             e.printStackTrace();
             Toast.makeText(getContext(), "Oops, something went wrong!", Toast.LENGTH_LONG).show();
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        ((MainActivity) getActivity()).getSupportActionBar().setTitle("My Banks");
-
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_banks, container, false);
-
+        return view;
     }
 }
